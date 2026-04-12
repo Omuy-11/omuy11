1fbf42187f00cb8dd92c33404e37cd592481d57f
Omuy-11 <omitmuya@gmail.com>
2026-04-12 20:26:34 +0700

    update

--- server.js
+++ server.js
@@ -9,19 +9,7 @@ app.use(express.static("public"));
 
 /* ================= DATABASE ================= */
 
-const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 /* ================= LOGIN ================= */
 
