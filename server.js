39399d287f20819118a9b66b6a9c5c0c8e5bcf57
Omuy-11 <omitmuya@gmail.com>
2026-04-12 19:56:36 +0700

    fix mysql pool

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ app.use(express.static("public"));
 
 /* ================= DATABASE ================= */
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
   ssl: {
     rejectUnauthorized: false
   }
